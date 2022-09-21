3af6b666e103801524f946f225a5ddb76c0371c3
l33ass <ilua-smirnof@yandex.ru>
2022-09-21 11:47:05 +0200

    Adding indexedDB to project

--- src/pages/collection/collection.js
+++ src/pages/collection/collection.js
@@ -1,4 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    class IsCollected {
+        constructor(id, collected) {
+            this.id = id;
+            this.collected = collected;
+        }
+    }
+
+    const request = indexedDB.open("MyTestDatabase", 3);
+    console.log(request);
+    
+    request.onerror = (event) => {
+        console.log(event.target.errorCode);
+    };
+    request.onsuccess = (event) => {
+        console.log("200")
+    };
+    request.onupgradeneeded = (event) => {
+        const db = event.target.result;
+        console.log(db);
+    }
+
+    
     document.querySelector('tbody').addEventListener('click', function (e) {
         if (e.target.tagName === "INPUT") {
             console.log(e.target);
